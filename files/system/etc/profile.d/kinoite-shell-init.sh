23894a6751b9c1f98b959535a5a3388fd094a60b
João Júnior <jbdsjunior@live.com>
2026-07-25 21:31:00 -0400

    Revert ""
    This reverts commit 140321309a0119518ce02c1a7320e0be65fe61e0.

--- files/system/etc/profile.d/kinoite-shell-init.sh
+++
@@ -1,9 +0,0 @@
-if command -v starship >/dev/null 2>&1; then
-  export STARSHIP_CONFIG="${STARSHIP_CONFIG:-/usr/share/starship/starship.toml}"
-  case "$-" in
-    *i*) eval "$(starship init bash 2>/dev/null)" ;;
-  esac
-fi
-if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv 2>/dev/null)"
-fi
